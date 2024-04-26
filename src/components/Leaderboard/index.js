5aa113fffc8b88ceb1d837699de128602eadef11
keerthana <busetty14keerthana@gmail.com>
2024-04-26 06:49:33 +0000

    Making-Apicall-part1

---
+++ src/components/Leaderboard/index.js
@@ -0,0 +1,64 @@
+import {useEffect, useState} from 'react'
+import Loader from 'react-loader-spinner'
+import {LeaderboardContainer, LoadingViewContainer} from './styledComponents'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  inProgress: 'IN_PROGRESS',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+}
+const Leaderboard = () => {
+  const [apiResponse, setApiResponse] = useState({
+    status: apiStatusConstants.initial,
+    data: null,
+    errorMsg: null,
+  })
+  useEffect(() => {
+    const getLeaderboardData = async () => {
+      setApiResponse({
+        status: apiStatusConstants.inProgress,
+        data: null,
+        errorMsg: null,
+      })
+      const url = 'https://apis.ccbp.in/leaderboard'
+      const options = {
+        method: 'GET',
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJhaHVsIiwicm9sZSI6IlBSSU1FX1VTRVIiLCJpYXQiOjE2MjMwNjU1MzJ9.D13s5wN3Oh59aa_qtXMo3Ec4wojOx0EZh8Xr5C5sRkU',
+        },
+      }
+      const response = await fetch(url, options)
+      const responseData = await response.json()
+    }
+    getLeaderboardData()
+  })
+  const renderLoadingView = () => (
+    <LoadingViewContainer>
+      <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
+    </LoadingViewContainer>
+  )
+  const renderSuccessView = () => {}
+  const renderFailureView = () => {}
+  // Your code goes here...
+
+  // Your code goes here...
+  const renderLeaderboard = () => {
+    const {status} = apiResponse
+    switch (status) {
+      case apiStatusConstants.inProgress:
+        return renderLoadingView()
+      case apiStatusConstants.success:
+        return renderSuccessView()
+      case apiStatusConstants.failure:
+        return renderFailureView()
+      default:
+        return null
+    }
+  }
+
+  return <LeaderboardContainer>{renderLeaderboard()}</LeaderboardContainer>
+}
+
+export default Leaderboard
